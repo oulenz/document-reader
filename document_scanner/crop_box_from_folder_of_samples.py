cefc07b582f498573d354032b426a7c63d616b2d
oulenz <oliver.urs.lenz@gmail.com>
2017-03-28 11:57:16 +0200

    small fixes due to earlier restructuring

--- document_scanner/crop_box_from_folder_of_samples.py
+++ document_scanner/crop_box_from_folder_of_samples.py
@@ -4,21 +4,22 @@ import os
 
 
 def process(config_path: str, image_path: str, debug: bool) -> None:
-    config = ds.get_config(config_path, image_path)
+    config = ds.parse_config(config_path, image_path)
     if debug:
         print(config)
 
     template = cv2.imread(config['template_path'], 0)
 
     if os.path.isdir(config['image_path']):
-        img_paths = [os.path.join(config['image_path'], x) for x in os.listdir(config['image_path']) if os.path.splitext(x)[1] in ['.jpg', '.png']]
+        img_paths = [os.path.join(config['image_path'], x) for x in os.listdir(config['image_path']) if os.path.splitext(x)[1] in ['.jpg', '.png', '.JPG']]
     else:
         img_paths = [config['image_path']]
     # print(len(img_paths))
+    boxes = ds.parse_boxes(config)
     for img_path in img_paths:
         print(img_path)
         scan = ds.find_document(template, img_path, debug)
-        ds.read_document(scan, template, img_path, config, debug)
+        ds.crop_boxes(scan, boxes, img_path, config)
     return
 
 
