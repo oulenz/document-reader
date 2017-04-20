8d3572ef5479c0cc9f31b966fe8b45816be84fd1
Oliver Urs Lenz <oliver.urs.lenz@gmail.com>
2017-04-20 15:48:17 +0200

    make moving of processed images in crop_and_save_boxes optional

--- document_scanner/crop_and_save_boxes.py
+++ document_scanner/crop_and_save_boxes.py
@@ -3,7 +3,7 @@ import document_scanner as ds
 import os
 
 
-def process(config_path: str, src_path: str, debug: bool) -> None:
+def process(config_path: str, src_path: str, debug: bool, move_processed: bool) -> None:
     if os.path.isdir(src_path):
         img_paths = [os.path.join(src_path, x) for x in os.listdir(src_path) if os.path.splitext(x)[1] in ['.jpg', '.png', '.JPG']]
     else:
@@ -29,10 +29,10 @@ def process(config_path: str, src_path: str, debug: bool) -> None:
             filename = os.path.join(output_path, file_stem + '_' + box_name + file_ext)
             print(filename)
             cv2.imwrite(filename, crop)
-        # move images that are done to subfolder to avoid having to redo them in case of restart
-        if not os.path.isdir(os.path.join(folder_path, 'processed')):
-            os.mkdir(os.path.join(folder_path, 'processed'))
-        os.rename(img_path, os.path.join(folder_path, 'processed', img_name))
+        if move_processed:
+            if not os.path.isdir(os.path.join(folder_path, 'processed')):
+                os.mkdir(os.path.join(folder_path, 'processed'))
+            os.rename(img_path, os.path.join(folder_path, 'processed', img_name))
     return
 
 
@@ -44,5 +44,7 @@ if __name__ == "__main__":
     parser.add_argument('image_path', help='The location of an image file or a folder of image files')
     parser.add_argument('--debug', dest='debug', action='store_true',
                         help='Display pictures and values')
+    parser.add_argument('--move_processed', dest='move_processed', action='store_true',
+                        help='Move images that have been processed to subfolder to avoid having to redo them in case of restart ')
     args = parser.parse_args()
-    process(args.config_path, args.image_path, args.debug)
+    process(args.config_path, args.image_path, args.debug, args.move_processed)
