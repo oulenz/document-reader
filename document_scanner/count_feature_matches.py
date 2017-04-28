9fe3ed92d6498cecf750d68d68ac60e8fbf0ae62
Oliver Urs Lenz <oliver.urs.lenz@gmail.com>
2017-04-28 09:21:43 +0200

    add single image option to count_feature_matches

--- document_scanner/count_feature_matches.py
+++ document_scanner/count_feature_matches.py
@@ -8,18 +8,23 @@ def process(config_path: str, src_path: str) -> None:
     config = ds.parse_config(config_path)
     template = cv2.imread(config['template_path'], 0)
 
-    feature_match_counts = []
+    if not os.path.isdir(src_path):
+        photo = cv2.imread(src_path, 0)
+        _, _, matches = ds.get_matching_points(template, photo, False)
+        print(len(matches))
+    else:
+        feature_match_counts = []
 
-    for label in [x for x in os.listdir(src_path) if os.path.isdir(os.path.join(src_path, x))]:
-        label_path = os.path.join(src_path, label)
-        for img_name in [x for x in os.listdir(label_path) if os.path.splitext(x)[1] in ['.jpg', '.png', '.JPG']]:
-            img_path = os.path.join(label_path, img_name)
-            print(img_path)
-            photo = cv2.imread(img_path, 0)
-            _, _, matches = ds.get_matching_points(template, photo, False)
-            feature_match_counts.append({'label': label, 'img_name': img_name, 'matches': len(matches)})
+        for label in [x for x in os.listdir(src_path) if os.path.isdir(os.path.join(src_path, x))]:
+            label_path = os.path.join(src_path, label)
+            for img_name in [x for x in os.listdir(label_path) if os.path.splitext(x)[1] in ['.jpg', '.png', '.JPG']]:
+                img_path = os.path.join(label_path, img_name)
+                print(img_path)
+                photo = cv2.imread(img_path, 0)
+                _, _, matches = ds.get_matching_points(template, photo, False)
+                feature_match_counts.append({'label': label, 'img_name': img_name, 'matches': len(matches)})
 
-    pd.DataFrame(feature_match_counts).to_csv(os.path.join(src_path, 'feature_match_counts.csv'), sep='|', index=False)
+        pd.DataFrame(feature_match_counts).to_csv(os.path.join(src_path, 'feature_match_counts.csv'), sep='|', index=False)
     return
 
 
