6cf89f84db7393d334356b68b009c60ecba4a167
Oliver Urs Lenz <oliver.urs.lenz@gmail.com>
2017-08-15 15:49:45 +0200

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='document_scanner',
+    version='0.0.1',
+    description='Extract content from a filled out document',
+    url='https://github.com/epigramai/document_scanner',
+    author='Oliver',
+    author_email='oliver@epigram.ai',
+    packages=find_packages('.')
+    # TODO install_requires=['tfwrapper>=0.1.0-rc5']
+)
